a6fbb0090d0d0098e33a9a6df091abe5c65c02c2
Ahmed Mohamed Khalifa <116463551+khalifacastaway11@users.noreply.github.com>
2024-06-06 11:17:44 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const app=express()
+const port=process.env.PORT || 3000
+const path=require('path')
+const http=require('http')
+const fs=require('fs')
+
+const sourcefolder=path.join(__dirname,"../")
+app.use(express.static(sourcefolder));
+app.get("/",(req,res)=>{
+    res.sendFile(sourcefolder+"/index.html")
+})
+app.get("/home", (req, res) => {
+  res.sendFile(sourcefolder + "/index.html");
+});
+app.get("/skills", (req, res) => {
+  res.sendFile(sourcefolder + "/skills.html");
+});
+
+
+app.get("/projects", (req, res) => {
+  res.sendFile(sourcefolder + "/projects.html");
+});
+
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    console.log(`Server running at port ${port}`)
+})
